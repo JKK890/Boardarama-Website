3df8fda205a61c6ed04d88e78e1c1d284c4dae45
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-01-16 16:48:05 -0400

    Auto base tag

--- src/my_business.php
+++ src/my_business.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/'; ?>">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="css/my_css.css">
+    <title>My Business</title>
 </head>
 <body>
-    
+    <h1>Hello, world!</h1>
 </body>
 </html>

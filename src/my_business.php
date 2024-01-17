497a1f6fd3d630e589931087e873cc42b802ccb6
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-01-17 17:59:06 -0400

    added header

--- src/my_business.php
+++ src/my_business.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/'; ?>">
-
-    <link rel="stylesheet" href="css/my_css.css">
-    <title>My Business</title>
-</head>
-<body>
-    <h1>Hello, world!</h1>
-</body>
+<html class="bg-gray-400 flex justify-center p-5" lang="en">
+    <?php include 'common/head.php';?>
+    <body class="max-w-5xl">
+        <?php include 'common/header.html';?>
+        <?php include 'common/taskbar.html';?>
+        <main>
+            <div>
+            <article>
+                <h3>
+                    Come play bored games and Eat!
+                </h3>
+                <p>
+                    Welcome to Boardarama - where good food meets great games! Our new spot
+                    in Halifax is all about tasty bites and board game delights. Grab a quick,
+                    delicious meal while playing your favorite games in a casual and welcoming
+                    atmosphere. Perfect for a fun night out with friends, a cozy date, or a 
+                    family-friendly gathering. Boardarama: redefining dining and gaming!
+                </p>
+                <p>
+                    Did you play a game here that you enjoyed and want to bring that experience home? 
+                    check out our e-store!
+                </p>
+            </article>
+            <div>
+                <div>
+                <img id="placeholder" alt="Picture of Catan"
+                    src="images/products/Nunchaku.jpg">
+                <footer>
+                    <h5>Newest Board Game Night</h5>
+                </footer>
+                </div>
+            </div>
+            </div>
+        </main>
+        <?php include 'common/footer.html';?>
+    </body>
 </html>
+

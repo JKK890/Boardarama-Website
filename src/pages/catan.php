7502e2a78f0e678f9dc3ac2ab0e8c18a35206710
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-01-27 20:08:43 -0400

    added all the class pages

---
+++ src/pages/catan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+
+<body>
+    <div class="content">
+        <div>
+            <?php include '../common/header.html'; ?>
+            <?php include '../common/taskbar.html'; ?>
+            <main class="prose max-w-none py-2">
+                <h1>Catan Classes (NEW!)</h1>
+                <p>
+                    Welcome to Boardarama, where strategic mastery meets the vibrant landscapes of Catan! Elevate your
+                    gaming experience with our Catan classes, as we bring the iconic board game to life in a whole new
+                    dimension. Settle, trade, and build your way to victory in this enthralling world of resource
+                    management and negotiation. Whether you're a seasoned settler or a newcomer to the world of Catan,
+                    our skilled instructors are here to guide you through the intricacies of the game, ensuring a
+                    dynamic and engaging learning experience. Join our community of enthusiastic settlers, and let the
+                    hexagonal tiles pave the way to friendly competition and strategic triumphs. Unleash your inner
+                    tactician and forge alliances in the pursuit of victory. Sign up for our Catan classes at
+                    Boardarama, where every roll of the dice is a step towards building your empire. Get ready to trade,
+                    build, and conquer at Boardarama!
+                </p>
+                <p>
+                    Ready to embark on a gaming adventure? To secure your spot in our thrilling D&D, Ticket to
+                    Ride, or Catan classes, simply visit any Boardarama location and sign up at the front desk. We look
+                    forward to
+                    welcoming you into our vibrant community of gamers, where every roll of the dice and strategic move
+                    creates unforgettable moments. Join us at Boardarama, where great games and good times await!
+                </p>
+            </main>
+        </div>
+        <?php include '../common/footer.html'; ?>
+    </div>
+
+</body>
+
+</html>
---
+++ src/pages/ticket-to-ride.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+
+<body>
+    <div class="content">
+        <div>
+            <?php include '../common/header.html'; ?>
+            <?php include '../common/taskbar.html'; ?>
+            <main class="prose max-w-none py-2">
+                <h1>Ticket to Ride Classes</h1>
+                <p>
+                    All aboard the excitement at Boardarama, where the journey begins with Ticket to Ride classes!
+                    Embark on a cross-country adventure in this iconic board game that blends strategy, planning, and a
+                    dash of competition. Explore the thrill of building train routes and connecting cities as you race
+                    against fellow enthusiasts to complete destination tickets. Whether you're a locomotive pro or new
+                    to the tracks, our seasoned instructors are here to guide you through the intricacies of Ticket to
+                    Ride, ensuring a delightful and strategic gaming experience. Join our community of rail tycoons and
+                    discover the joy of planning your routes, blocking opponents, and claiming victory. Sign up for our
+                    Ticket to Ride classes at Boardarama and let the colorful train cars pave the way to an
+                    unforgettable gaming expedition. All aboard for fun and friendly competition - your ticket to
+                    excitement awaits!
+                </p>
+                <p>
+                    Ready to embark on a gaming adventure? To secure your spot in our thrilling D&D, Ticket to
+                    Ride, or Catan classes, simply visit any Boardarama location and sign up at the front desk. We look
+                    forward to
+                    welcoming you into our vibrant community of gamers, where every roll of the dice and strategic move
+                    creates unforgettable moments. Join us at Boardarama, where great games and good times await!
+                </p>
+            </main>
+        </div>
+        <?php include '../common/footer.html'; ?>
+    </div>
+
+</body>
+
+</html>

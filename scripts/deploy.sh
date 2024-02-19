1a19490255a79bf7dcbb5a3158f28b6a161fff2b
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-02-19 16:55:11 -0400

    fix to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 ssh-keyscan $SERVER_HOSTNAME >> ~/.ssh/known_hosts
 ssh $SERVER_USER@$SERVER_HOSTNAME "rm -rf ~/public_html/staged/*"
 scp -r ./src/* $SERVER_USER@$SERVER_HOSTNAME:~/public_html/staged
-expect "u21@************'s password:"
-send "alreadyDECIDED78\r"

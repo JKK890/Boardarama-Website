2d19ed035a4769f8a18691921356b5d53bd47ded
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-04-12 14:37:14 -0300

    code clean up

--- src/pages/sitemap.php
+++ src/pages/sitemap.php
@@ -14,7 +14,6 @@
                     </div>
                     <div class="bg-gray-400 rounded flex-grow p-4 text-2xl flex flex-col gap-4">
                         <div class="text-3xl">Shop</div>
-                        <a href="pages/estore.php">e-store&nbsp;Options</a>
                         <a href="pages/products.php">Product&nbsp;Catalog</a>
                         <a href="pages/register.php">Register</a>
                         <a href="pages/cart.php">Shopping&nbsp;Cart</a>
